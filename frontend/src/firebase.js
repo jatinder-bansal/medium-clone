cfc1494671ba4d6c25a830659a9d81f56452fa91
jatinder-bansal <jatinder1901.be20@chitkara.edu.in>
2023-06-06 11:23:13 +0530

    Some Changes

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -9,13 +9,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB9ENM2igTBlfM78UhC-oJ12AJMoh4mvxw",
-  authDomain: "medium-clone-20602.firebaseapp.com",
-  projectId: "medium-clone-20602",
-  storageBucket: "medium-clone-20602.appspot.com",
-  messagingSenderId: "915318857508",
-  appId: "1:915318857508:web:3824f97f8093329288ddb1",
-  measurementId: "G-RNFB1YRT5V",
+  apiKey: "AIzaSyC7-81csRzSGDKu_GvAzxZ1SfgdBKUVJfw",
+  authDomain: "mediumclone-4707c.firebaseapp.com",
+  projectId: "mediumclone-4707c",
+  storageBucket: "mediumclone-4707c.appspot.com",
+  messagingSenderId: "91400864150",
+  appId: "1:91400864150:web:b29a79f875245a094fddb7"
 };
 
 // Initialize Firebase
